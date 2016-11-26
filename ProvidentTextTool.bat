8399a996815587b0af13c24d436b71aa89362afa
familymcqueens <familymcqueens@gmail.com>
2016-11-26 10:30:34 -0800

    Add files via upload

--- ProvidentTextTool.bat
+++ ProvidentTextTool.bat
@@ -1,4 +1,7 @@
 @echo off
+tasklist /FI "IMAGENAME eq wwebserver.exe" 2>NUL | find /I /N "wwebserver.exe">NUL
+if not "%ERRORLEVEL%"=="0" (c:\Wwebserver\wwebserver.exe /q /c /min)
+
 c:\Windows\System32\cmd.exe /q /c start /min ProvidentTextTool_1.pl 
 start chrome %CD%\ProvidentTextTool.html
 
